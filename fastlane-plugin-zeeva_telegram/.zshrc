21210ce85b7c9372f90075344258ab1561ddb745
Yazan Tarifi <yazantarifi989@gmail.com>
2022-09-26 00:46:11 +0300

    Add Send Message to Channel Id Request

--- fastlane-plugin-zeeva_telegram/.zshrc
+++ fastlane-plugin-zeeva_telegram/.zshrc
@@ -18,3 +18,105 @@ export NVM_DIR=~/.nvm
 source $(brew --prefix nvm)/nvm.sh
 export NVM_DIR=~/.nvm
 source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
--- fastlane-plugin-zeeva_telegram/lib/fastlane/plugin/zeeva_telegram/actions/zeeva_telegram_action.rb
+++ fastlane-plugin-zeeva_telegram/lib/fastlane/plugin/zeeva_telegram/actions/zeeva_telegram_action.rb
@@ -1,11 +1,70 @@
 require 'fastlane/action'
+require 'net/http'
 require_relative '../helper/zeeva_telegram_helper'
 
 module Fastlane
   module Actions
     class ZeevaTelegramAction < Action
       def self.run(params)
-        UI.message("The zeeva_telegram plugin is working!")
+        serverToken = params[:server_token]
+        buildName = params[:build_name]
+        buildType = params[:build_type]
+        platformCode = params[:platform_code]
+        chatId = params[:chat_id]
+        title = params[:title]
+        message = params[:message]
+
+        buildContent = ""
+
+        begin
+          File.open 'zeevaBuild.md' do |f|
+            file_content = File.read('zeevaBuild.md');
+            buildContent = file_content
+          end
+        rescue Errno::ENOENT => e
+          $stderr.puts "Caught the exception: #{e}"
+        end
+
+        UI.message("Zeeva Fastlane Plugin - Discord Configuration - Started !!")
+        UI.message("=========================== Bot Credentials ===========================")
+        UI.message("Server Token : " + serverToken)
+        UI.message("Server Chat Id : " + chatId)
+        UI.message("=======================================================================")
+        run_application_bot(serverToken, chatId, get_message_body(
+          buildName,
+          buildType,
+          platformCode,
+          buildContent,
+          title,
+          message
+        ))
+      end
+
+      def self.run_application_bot(token, chatId, messageBody)
+        Net::HTTP.get(URI.parse('https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + chatId + '&text=' + messageBody))
+      end
+
+      def self.get_message_body(buildName, buildType, platform, buildContent, title, messageContent)
+        message = "====== New Build ====== \n"
+        message += "\n"
+        message += "Build Name : " + buildName + " \n"
+        message += "Build Type : " + buildType + " \n"
+        message += "Build Platform : " + platform + " \n"
+        if buildContent.length != 0
+          message += "Build Content : " + " \n"
+          message += buildContent + " \n"
+        end
+
+        if title.length != 0
+          message += "Title : " + title + " \n"
+        end
+
+        if messageContent.length != 0
+          message += "Message : " + messageContent + " \n"
+        end
+
+        message += "====== New Build ======"
+        return message
       end
 
       def self.description
@@ -27,19 +86,60 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "ZEEVA_TELEGRAM_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(
+            key: :server_token,
+            env_name: "SERVER_TOKEN",
+            description: "The Private Current Server Token",
+            optional: false,
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :build_name,
+            env_name: "BUILD_NAME",
+            description: "The Build Name that Currently Running",
+            optional: false,
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :build_type,
+            env_name: "BUILD_TYPE",
+            description: "The Build Type that Currently Running",
+            optional: false,
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :platform_code,
+            env_name: "PLATFORM_CODE",
+            description: "The Platform that Currently Running Ex: Android, IOS, Mac",
+            optional: false,
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :title,
+            env_name: "TITLE",
+            description: "The Title of the Build",
+            optional: false,
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :message,
+            env_name: "MESSAGE",
+            description: "The Message Body of the Build",
+            optional: false,
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :chat_id,
+            env_name: "CHAT_ID",
+            description: "The Chat Id of the Message",
+            optional: false,
+            type: String
+          )
         ]
       end
 
       def self.is_supported?(platform)
-        # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
-        # See: https://docs.fastlane.tools/advanced/#control-configuration-by-lane-and-by-platform
-        #
-        # [:ios, :mac, :android].include?(platform)
+        [:ios, :mac, :android].include?(platform)
         true
       end
     end
